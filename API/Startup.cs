fca285dc7b922cba4ff4caa51beada9615313add
Md Ahsanul Hoque <ahrony90@gmail.com>
2021-02-22 19:54:23 -0400

    Add ApplicaitonServiceExtention class for startup class simpliciyt

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Extentions;
 using Application.Core;
 using Application.Doctors;
 using MediatR;
@@ -31,28 +32,8 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-               {
-                   opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
-
-               }
-            );
-            services.AddCors(opt =>
-            {
-                opt.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-                });
-            });
-            services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+            services.AddApplicationServices(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
